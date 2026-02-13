b39dd2d5b4622af0401e6a94e6f356070388181e
Donald Sharp <sharpd@nvidia.com>
2026-02-13 15:27:57 -0500

    bgpd: Do not count connection as stalled incorrectly on peer startup
    I am seeing this error message on peer startup:
    
    2026/02/13 14:59:51 BGP: [T83RR-8SM5G] bgpd 10.7.0-dev starting: vty@2605, bgp@<all>:179
    2026/02/13 14:59:51 BGP: [G822R-SBMNH] config-from-file# router bgp 65002
    2026/02/13 14:59:51 BGP: [G822R-SBMNH] config-from-file#  no bgp ebgp-requires-policy
    2026/02/13 14:59:51 BGP: [G822R-SBMNH] config-from-file#  neighbor 192.168.1.1 remote-as external
    2026/02/13 14:59:51 BGP: [G822R-SBMNH] config-from-file#  address-family ipv4 unicast
    2026/02/13 14:59:51 BGP: [G822R-SBMNH] config-from-file#   neighbor 192.168.1.1 capability orf prefix-list both
    2026/02/13 14:59:51 BGP: [G822R-SBMNH] config-from-file#   neighbor 192.168.1.1 prefix-list r1 in
    2026/02/13 14:59:51 BGP: [G822R-SBMNH] config-from-file#  exit-address-family
    2026/02/13 14:59:51 BGP: [G822R-SBMNH] config-from-file# !
    2026/02/13 14:59:51 BGP: [G822R-SBMNH] config-from-file# ip prefix-list r1 seq 5 permit 10.10.10.1/32
    2026/02/13 14:59:51 BGP: [M7Q4P-46WDR] vty[24]@> enable
    2026/02/13 14:59:51 BGP: [M7Q4P-46WDR] vty[24]@> enable
    2026/02/13 14:59:51 BGP: [M7Q4P-46WDR] vty[24]@# clear log cmdline-targets
    2026/02/13 14:59:51 BGP: [M7Q4P-46WDR] vty[24]@(config)# log commands
    2026/02/13 14:59:51 BGP: [M7Q4P-46WDR] vty[24]@(config)# log timestamp precision 6
    2026/02/13 14:59:51.204269 BGP: [M7Q4P-46WDR] vty[5]@> enable
    2026/02/13 14:59:51.204599 BGP: [M7Q4P-46WDR] vty[5]@# send log level info === TEST-START: bgp_orf/test_bgp_orf.py::test_bgp_orf
    2026/02/13 14:59:51.204639 BGP: === TEST-START: bgp_orf/test_bgp_orf.py::test_bgp_orf
    2026/02/13 14:59:51.246430 BGP: [M7Q4P-46WDR] vty[5]@> enable
    2026/02/13 14:59:51.246861 BGP: [M7Q4P-46WDR] vty[5]@# do show logging
    2026/02/13 14:59:51.617064 BGP: [JQ5A9-TEQYM][EC 33554512] 192.168.1.1(r1) has not made any SendQ progress for 2 holdtimes (360s), terminating session
    
    Notice that bgpd is starting at 14:59:51 and is attempting to start a connection and immediately decides that
    no SendQ progress has been made at all and aborts the connection.
    
    This is a timing issue where if we add a packet before the io pthread has had a chance to
    send any packet at all the last_sendq_ok time will never have been set and the subtraction
    in bgp_packet_add for the delta will generate a very large value for delta and cause
    the test for delta being greater than sendholdtime, causing the connection to be reset.
    
    Modify the code such that upon starting a connection or accepting a connection set the
    last_sendq_ok value to a lower bounds such that this will not cause a problem with a false
    positive about the last time a packet was sent.
    
    Signed-off-by: Donald Sharp <sharpd@nvidia.com>

--- bgpd/bgp_network.c
+++ bgpd/bgp_network.c
@@ -608,6 +608,9 @@ static void bgp_accept(struct event *event)
 
 		if (dynamic_peer) {
 			incoming = dynamic_peer->connection;
+
+			incoming->last_sendq_ok = monotime(NULL);
+
 			/* Dynamic neighbor has been created, let it proceed */
 			incoming->fd = bgp_sock;
 
@@ -767,6 +770,7 @@ static void bgp_accept(struct event *event)
 	incoming->fd = bgp_sock;
 	incoming->su_local = sockunion_getsockname(incoming->fd);
 	incoming->su_remote = sockunion_dup(&su);
+	incoming->last_sendq_ok = monotime(NULL);
 
 	if (bgp_set_socket_ttl(incoming) < 0)
 		if (bgp_debug_neighbor_events(doppelganger))
@@ -907,6 +911,8 @@ enum connect_result bgp_connect(struct peer_connection *connection)
 
 	assert(!CHECK_FLAG(connection->thread_flags, PEER_THREAD_WRITES_ON));
 	assert(!CHECK_FLAG(connection->thread_flags, PEER_THREAD_READS_ON));
+
+	connection->last_sendq_ok = monotime(NULL);
 
 	if (peer->bgp->router_id.s_addr == INADDR_ANY) {
 		peer_set_last_reset(peer, PEER_DOWN_ROUTER_ID_ZERO);
