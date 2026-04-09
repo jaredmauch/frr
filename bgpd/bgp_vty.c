354aac38d3745185a77ea2ff6a5d7ce8e72e42ec
souroy <souroy@nvidia.com>
2026-04-09 13:34:28 -0700

    bgpd: fix last Reset timer losing day part after 24 hours
    The lastResetTimerMsecs JSON field was computed using gmtime_r() and
    only accounted for hours, minutes, and seconds (tm_hour, tm_min,
    tm_sec), but missed the day field (tm_yday). This caused the value
    to wrap around and lose days when elapsed time exceeded 24 hours.
    Use direct multiplication (uptime * 1000) instead, matching how
    bgpTimerUpMsec is correctly computed.
    
    Signed-off-by: Soumya Roy <souroy@nvidia.com>

--- bgpd/bgp_vty.c
+++ bgpd/bgp_vty.c
@@ -15809,7 +15809,6 @@ static void bgp_show_peer(struct vty *vty, struct peer *p, uint16_t sh_flags, bo
 	if (show_brief) {
 		if (use_json) {
 			time_t uptime;
-			struct tm tm;
 
 			if (p->hostname)
 				json_object_string_add(json_neigh, "hostname", p->hostname);
@@ -15827,10 +15826,9 @@ static void bgp_show_peer(struct vty *vty, struct peer *p, uint16_t sh_flags, bo
 
 			uptime = monotime(NULL);
 			uptime -= p->resettime;
-			gmtime_r(&uptime, &tm);
+
 			json_object_int_add(json_neigh, "lastResetTimerMsecs",
-					    (tm.tm_sec * 1000) + (tm.tm_min * 60000) +
-						    (tm.tm_hour * 3600000));
+					    (int64_t)uptime * 1000);
 			json_stat = json_object_new_object();
 			json_object_int_add(json_stat, "totalSent", PEER_TOTAL_TX(p));
 			json_object_int_add(json_stat, "totalRecv", PEER_TOTAL_RX(p));
@@ -17364,16 +17362,12 @@ static void bgp_show_peer(struct vty *vty, struct peer *p, uint16_t sh_flags, bo
 	} else {
 		if (use_json) {
 			time_t uptime;
-			struct tm tm;
 
 			uptime = monotime(NULL);
 			uptime -= p->resettime;
-			gmtime_r(&uptime, &tm);
 
 			json_object_int_add(json_neigh, "lastResetTimerMsecs",
-					    (tm.tm_sec * 1000)
-						    + (tm.tm_min * 60000)
-						    + (tm.tm_hour * 3600000));
+					    (int64_t)uptime * 1000);
 			bgp_show_peer_reset(NULL, p, json_neigh, true);
 		} else {
 			vty_out(vty, "  Last reset %s, ",
